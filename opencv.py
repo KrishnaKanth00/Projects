b32642b54a98f3a263ad38d98e44ff1bdd097c30
KrishnaKanth00 <160703341+KrishnaKanth00@users.noreply.github.com>
2025-11-27 12:06:08 +0530

    Corner Detection tutorial model with line connecting them

--- opencv.py
+++ opencv.py
@@ -1,19 +1,25 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+img = cv2.imread('WIN_20251127_11_02_34_Pro.jpg')
+img = cv2.resize(img,(0,0),fx=.5,fy=.5)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    corners = cv2.goodFeaturesToTrack(gray,100,.5,25)
-    corners = np.int_(corners)
+corners = cv2.goodFeaturesToTrack(gray,100,.2,10)
+corners = np.int_(corners)
 
-    for corner in corners:
-        x,y = corner.ravel()
-        cv2.circle(frame,(x,y),10,(0,0,255),1)
-    cv2.imshow('gray',frame)
+for corner in corners:
+    x,y = corner.ravel()
+    cv2.circle(img,(x,y),10,(0,0,255),1)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+for i in range(len(corners)):
+    for j in range(i+1,len(corners)):
+        start = tuple(corners[i][0])
+        end = tuple(corners[j][0])
+        color = tuple(map(int,np.random.randint(0,255,size=3)))
+        cv2.line(img,start,end,color,2)
+cv2.imshow('gray',img)
+
+cv2.waitKey(0)
 cv2.destroyAllWindows()
