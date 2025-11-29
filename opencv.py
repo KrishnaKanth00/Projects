b43ed40f7e375343fc4717562f8bb564885cb330
KrishnaKanth00 <160703341+KrishnaKanth00@users.noreply.github.com>
2025-11-29 15:30:54 +0530

    eye detection

--- opencv.py
+++ opencv.py
@@ -2,6 +2,7 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 while True:
     ret,frame = cap.read()
@@ -9,7 +10,12 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray,1.1,5)
     for x,y,w,h in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_color = frame[y:y+h,x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray,1.1,5)
+        for ex,ey,ew,eh in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),3)
+
 
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) == ord('q'):
