fa840589992304e54f9b7d64b9af1d221c088f41
KrishnaKanth00 <160703341+KrishnaKanth00@users.noreply.github.com>
2025-11-30 11:39:38 +0530

    Hand Tracking using Mediapipe

--- opencv.py
+++ opencv.py
@@ -1,13 +1,25 @@
 import cv2
+import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
-
 prev_time = 0
 curr_time = 0
 
+
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     net , frame = cap.read()
+    rgbImg = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    results = hands.process(rgbImg)
+
+    if results.multi_hand_landmarks is not None:
+        for handsLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(frame,handsLms,mphands.HAND_CONNECTIONS)
+
 
     curr_time = time.time()
     fps = 1/ (curr_time-prev_time)
