a4ebf19d60cdc2dae11048cda3700823edde50a8
KrishnaKanth00 <160703341+KrishnaKanth00@users.noreply.github.com>
2025-11-30 11:19:02 +0530

    Showing frame rate

--- opencv.py
+++ opencv.py
@@ -1,32 +1,24 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
-model = "face_detection_yunet_2023mar.onnx"
-detector = cv2.FaceDetectorYN_create(model,"",(640,480))
+
+prev_time = 0
+curr_time = 0
+
 while True:
-    ret,frame = cap.read()
-    detector.setInputSize((frame.shape[1], frame.shape[0]))
+    net , frame = cap.read()
 
-    faces = detector.detect(frame)
+    curr_time = time.time()
+    fps = 1/ (curr_time-prev_time)
+    prev_time = curr_time
 
-    if faces[1] is not None:
-        for face in faces[1]:
-            x,y,w,h = face[:4].astype(int)
-            lx,ly,rx,ry = face[4:8].astype(int)
-
-            cv2.circle(frame,(lx,ly),15,(0,255,0),2)
-            cv2.circle(frame, (rx, ry), 15, (0, 255, 0), 2)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-
-
-
+    cv2.putText(frame,str(int(fps)),(20,50),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),3)
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
 cv2.destroyAllWindows()
-cap.release()
-
 
 
 
